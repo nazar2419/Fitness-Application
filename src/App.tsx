62e0ccbb0093cbb0292442cc65ba7d3bec629cfb
nazar2419 <nazarx095@gmail.com>
2023-03-10 23:17:31 +0200

    benefit page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
+import Benefits from "@/scenes/benefits";
 import { useState, useEffect} from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -28,6 +29,8 @@ function App() {
         setSelectedPage={setSelectedPage}
       />
       <Home setSelectedPage={setSelectedPage}/>
+      <Benefits setSelectedPage={setSelectedPage}/>
+
     </div>
   )
 }
